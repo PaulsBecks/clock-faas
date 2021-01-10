91653451ffe52f029e3fe16ce7b9c7c29e879633
Paul Beck <paulbeck@Pauls-MBP.fritz.box>
2021-01-10 17:41:24 +0100

    add function

---
+++ index.js
@@ -0,0 +1,12 @@
+const main = async (req, res) => {
+    const d = new Date()
+    const localDateString = d.toLocaleString('de-DE', { timeZone: 'Europe/Berlin' })
+    const [hours, minutes, seconds] = localDateString.split(", ")[1].split(":")
+    text = hours + ":" + minutes
+    symbol = "0111111010001001100010011000100110010001101000011000000101111110"
+    console.log("Current time " + text)
+    res.json({ text, symbol })
+}
+
+module.exports = main
+
