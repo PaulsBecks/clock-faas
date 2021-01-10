851270c789ecefab982defd6bb05bdae9d7f7dfd
Paul Beck <paulbeck@Pauls-MBP.fritz.box>
2021-01-10 17:49:37 +0100

    24 hours time and space

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const main = async (req, res) => {
     const d = new Date()
-    const localDateString = d.toLocaleString('de-DE', { timeZone: 'Europe/Berlin' })
-    const [hours, minutes, seconds] = localDateString.split(", ")[1].split(":")
-    text = hours + ":" + minutes
+    const localDateString = d.toLocaleString('de-DE', { timeZone: 'Europe/Berlin', hour12: false })
+    const [hours, minutes] = localDateString.split(", ")[1].split(":")
+    text = " " + hours + ":" + minutes
     symbol = "0111111010001001100010011000100110010001101000011000000101111110"
     console.log("Current time " + text)
     res.json({ text, symbol })
